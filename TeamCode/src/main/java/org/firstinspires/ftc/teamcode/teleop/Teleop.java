589b3cdd9c14ef0454126172527cfd93cd44f308
Breichen1 <reichenmiikka@gmail.com>
2024-07-14 03:15:32 -0500

    Update Teleop.java
    Added manual arm control toggle for adjustments without PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -38,13 +38,30 @@ public class Teleop extends LinearOpMode {
         //Run immediately when starting
         while(!isStopRequested()) {
             //Periodic Opmode
-            
+            telemetry.addData(
+                    "Periodic currently running",
+                    "Operator can hold left bumper for manual arm control");
+
             //Drivetrain method
             m_Drive.Drive(Driver.getLeftX(), Driver.getLeftY(), Driver.getRightX(), Driver.getButton(GamepadKeys.Button.RIGHT_BUMPER));
 
             //Superstructure preset - Zero everything
             if (Operator.getButton(GamepadKeys.Button.BACK)) {
                 m_Superstructure.zeroPreset();
+            }
+
+            //Superstructure manual input toggle - Triggered by holding holding left bumper
+            if (Operator.getButton(GamepadKeys.Button.LEFT_BUMPER)) {
+                m_Superstructure.ManualInput(
+                        Operator.getLeftY(),
+                        Operator.getButton(GamepadKeys.Button.A),
+                        Operator.getButton(GamepadKeys.Button.B),
+                        Operator.getButton(GamepadKeys.Button.X)
+                );
+                telemetry.addData(
+                        "MANUAL INPUT ENABLED",
+                        "A = ARM, B = WRIST, X = ELEVATOR." + "USE LEFT STICK TO CONTROL INPUT");
+
             }
 
             //IMU Reset button
