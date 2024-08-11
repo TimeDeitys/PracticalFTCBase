8d309ebc5ece897e5a630f328844f339ad75a892
Breichen <119686482+Breichen1@users.noreply.github.com>
2024-08-11 10:17:55 -0500

    Created custom auto drive (Drivebot 2)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ApriltagUSBCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ApriltagUSBCamera.java
@@ -22,6 +22,14 @@ public class ApriltagUSBCamera {
     private Telemetry telemetry;
     private HardwareMap Map;
 
+    public double TagX;
+    public double TagY;
+    public double TagZ;
+
+    public double TagInches;
+    public double TagAngle;
+    public double TagID;
+
     public ApriltagUSBCamera(HardwareMap Map, Telemetry telemetry) {
         this.telemetry = telemetry;
         this.Map = Map;
@@ -108,6 +116,13 @@ public class ApriltagUSBCamera {
                 telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
                 telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
                 telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                TagX = detection.ftcPose.x;
+                TagY = detection.ftcPose.x;
+                TagZ = detection.ftcPose.x;
+
+                TagInches = detection.ftcPose.range;
+                TagAngle = detection.ftcPose.yaw;
+
             } else {
                 telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
                 telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MecanumDriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MecanumDriveSubsystem.java
@@ -1,13 +1,19 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Const;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -21,6 +27,8 @@ public class MecanumDriveSubsystem {
     private Motor leftBack;
     private Motor rightBack;
     private Telemetry telemetry;
+
+    private ElapsedTime runtime = new ElapsedTime();
 
     private double IMUOffset;
 
@@ -73,9 +81,90 @@ public class MecanumDriveSubsystem {
         IMUOffset = imu.getAbsoluteHeading();
     }
 
+    public int getForwardTicks(){
+        //assumes Forward deadwheel is plugged into LeftFront
+        return leftFront.getCurrentPosition();
+    }
+
+    public int getStrafeTicks(){
+        //assumes Forward deadwheel is plugged into RightBack
+        return rightBack.getCurrentPosition();
+    }
+
+    public void resetDriveEncoders() {
+        leftFront.stopAndResetEncoder();
+        rightBack.stopAndResetEncoder();
+    }
+
     public void drivePeriodic() {
         telemetry.addData("Heading", getHeading());
         //Called once per scheduler run
         //PUT PERIODIC HERE
+    }
+
+    //Drivebot scheduler: a custom movement utility.
+
+    //finds the amount of ticks to move for a given distance in inches
+    public static double driveDistance(double distance) {
+        double drive = (Constants.AutoConstants.COUNTS_PER_INCH);
+        int outputTicks = (int) Math.floor(drive * distance);
+        return outputTicks;
+    }
+
+    /**
+     * Autonomously drive robot centric.
+     * @param Forward forward/backward in inches (forward is positive)
+     * @param Right Right/left in inches (Right is positive)
+     * @param TimeoutS  Allowed time to run command
+     */
+    public void AutoDriveRC(double Forward, double Right, double TimeoutS) {
+        double initialHeading = getHeading();
+
+        int ForwardTarget;
+        int StrafeTarget;
+
+        if(linearOpMode.opModeIsActive()) {
+
+            //Create PID constants
+            PIDCoefficients TC = Constants.AutoConstants.TranslationPID;
+            PIDCoefficients SC = Constants.AutoConstants.StrafePID;
+            PIDCoefficients HC = Constants.AutoConstants.HeadingPID;
+
+            PIDController TranslationController = new PIDController(TC.p, TC.i, TC.d);
+            PIDController StrafeController = new PIDController(SC.p, SC.i, SC.d);
+            PIDController HeadingController = new PIDController(HC.p, HC.i, HC.d);
+
+            TranslationController.setTolerance(Constants.AutoConstants.PIDTolerance);
+            StrafeController.setTolerance(Constants.AutoConstants.PIDTolerance);
+
+            //set target positions
+            ForwardTarget = (int) driveDistance(Forward);
+            StrafeTarget = (int) driveDistance(Right);
+
+            runtime.reset();
+
+            StrafeController.setSetPoint(StrafeTarget);
+            TranslationController.setSetPoint(ForwardTarget);
+
+            while(linearOpMode.opModeIsActive() &&
+                    (runtime.seconds() < TimeoutS) &&
+            !TranslationController.atSetPoint() && !StrafeController.atSetPoint() ) {
+                //Drivebot Periodic
+                //actually drives the robot.
+                DriveRobotRelative(StrafeController.calculate(getStrafeTicks(), StrafeTarget), TranslationController.calculate(getForwardTicks(), ForwardTarget), HeadingController.calculate(getHeading(), initialHeading), false);
+                telemetry.addData("AUTO DRIVE STATUS", "RUNNING");
+                telemetry.addData("X Travelled;", getForwardTicks());
+                telemetry.addData("Y Travelled;", getStrafeTicks());
+                telemetry.addData("Heading;", getHeading());
+            }
+
+            //Stop all motion
+            DriveRobotRelative(0,0,0, false);
+            resetDriveEncoders();
+
+
+
+
+        }
     }
 }
