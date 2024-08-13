340228050850f553770c7a4598c40b9f4f0e92a0
AxtonWelch <axtonwelch@gmail.com>
2024-08-12 19:37:12 -0500

    Create DriveTrainBase.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DriveTrainBase.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.subsystems.MecanumDriveSubsystem;
+
+@TeleOp(name = "Drive Train Base")
+public class DriveTrainBase extends LinearOpMode {
+
+    //Gamepad bindings
+    private GamepadEx Driver;
+    private GamepadEx Operator;
+
+
+    //subsystems
+    private MecanumDriveSubsystem m_Drive;
+
+
+
+    @Override
+    public void runOpMode() {
+        //Run when initializing
+        m_Drive = new MecanumDriveSubsystem(hardwareMap, telemetry);
+
+        Driver = new GamepadEx(gamepad1);
+
+        telemetry.update();
+        waitForStart();
+        //Run immediately when starting
+
+
+        while (opModeIsActive()) {
+                //Periodic Opmode
+                telemetry.addData(
+                        "Periodic currently running",
+                        "Operator can hold left bumper for manual arm control");
+
+
+                //IMU Reset button
+                if (Driver.getButton(GamepadKeys.Button.Y)) {
+                   m_Drive.resetHeading();
+                }
+
+                //Drivetrain method
+                m_Drive.Drive(Driver.getLeftX(), Driver.getLeftY(), Driver.getRightX(), Driver.getButton(GamepadKeys.Button.RIGHT_BUMPER));
+
+
+                //Superstructure manual input toggle - Triggered by holding holding left bumper
+
+
+
+
+                telemetry.update();
+            }
+        }
+    }
+
